1f399ce8c5c7005ef6d4d09772e7c9b34e827ba3
Ranjan098 <144373098+Ranjan098@users.noreply.github.com>
2023-11-08 21:01:42 +0530

    Update lo.py

--- lo.py
+++ lo.py
@@ -70,9 +70,9 @@ def pull_run(work, cmds):
 	        print(result) 
 bot = Client(
   "CW",
-  bot_token="6119182235:AAHtHLm2Vnn_K9HYzaKc5DfAXVbGxwWQx0o",
-  api_id=28473180,
-  api_hash="60cc753767c22c5b0529daf633b3a02e"
+  bot_token="6453584668:AAG2cZyOP-U-QwFDUNAs6QyEoNnblsdgROc",
+  api_id=12606917, 
+  api_hash="f25113b8c17dca6fa7abda53a86bd4f7"
 )
 
 @bot.on_message(filters.command(["down"]) & ~filters.edited)
